f77193ff77f98c64deb8c60808063f87feb7318f
Fontidar <joliverchin@gmail.com>
2026-05-13 12:33:35 +0200

    v1.1.1

---
+++ Assets/Scripts/Close.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    public void CloseApp()
+    {
+        // Cierra la aplicación compilada de forma inmediata
+        Application.Quit();
+    }
+}
